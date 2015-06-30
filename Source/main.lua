849d83e8b1eceda3ab19a72ae082e8559bdf04b5
Beluki <beluki@gmx.com>
2015-06-30 09:20:49 +0200

    Append Love2D version to the window title.

--- Source/main.lua
+++ Source/main.lua
@@ -8,6 +8,12 @@ require 'game/game'
 
 -- Initialization:
 function love.load ()
+
+    -- include Love version in the window title:
+    local major, minor, revision, codename = love.getVersion()
+    love.window.setTitle(string.format("dir (love2d %d.%d.%d)", major, minor, revision))
+
+    -- initialize seed:
     math.randomseed(os.time())
 
     local game = Game()
