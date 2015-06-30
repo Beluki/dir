527fee98b8645f717d32e8f4679ede3940172dc2
Beluki <beluki@gmx.com>
2015-06-30 11:57:14 +0200

    Change icon from the windows stub.

--- Windows/build.sh
+++ Windows/build.sh
@@ -5,7 +5,6 @@
 # Requirements:
 
 # C:\MinGW32 and C:\MinGW64 contain MinGW for x86 and x86-64.
-# C:\ResourceHacker contains Resource Hacker.
 # wget and 7z (7-zip) are available in the current $PATH.
 
 # Example:
@@ -13,59 +12,61 @@
 # ./build.sh "dir 2015.06.29"
 
 
+LOVE_WIN32_URL="https://bitbucket.org/rude/love/downloads/love-0.9.2-win32.zip"
+LOVE_WIN64_URL="https://bitbucket.org/rude/love/downloads/love-0.9.2-win64.zip"
+
 TARGET="$1"
+TARGET_WIN32="$TARGET (win32)"
+TARGET_WIN64="$TARGET (win64)"
 
 
-echo "Creating target folders... "
+echo "Creating .love file..."
 
-mkdir "$TARGET (win32)"
-mkdir "$TARGET (win64)"
-
-
-echo "Downloading Love2D..."
-
-mkdir "$TARGET (win32)/love2d"
-mkdir "$TARGET (win64)/love2d"
-
-wget --no-check-certificate https://bitbucket.org/rude/love/downloads/love-0.9.2-win32.zip -O "love-win32.zip"
-wget --no-check-certificate https://bitbucket.org/rude/love/downloads/love-0.9.2-win64.zip -O "love-win64.zip"
-
-7z e love-win32.zip -o"$TARGET (win32)/love2d"
-7z e love-win64.zip -o"$TARGET (win64)/love2d"
+pushd .
+cd ../Source
+7z a ../Windows/dir.love -r . -tzip
+popd
 
 
-echo "Overwriting Love2D icons..."
+echo "Creating target folders..."
 
-/c/ResourceHacker/ResourceHacker.exe -addoverwrite "$TARGET (win32)/love2d/love.exe", "$TARGET (win32)/love2d/love.exe", "dir.ico", ICONGROUP, MAINICON, 0
-/c/ResourceHacker/ResourceHacker.exe -addoverwrite "$TARGET (win64)/love2d/love.exe", "$TARGET (win64)/love2d/love.exe", "dir.ico", ICONGROUP, MAINICON, 0
+mkdir "$TARGET_WIN32" "$TARGET_WIN32/dir" "$TARGET_WIN32/love2d"
+mkdir "$TARGET_WIN64" "$TARGET_WIN64/dir" "$TARGET_WIN64/love2d"
 
 
 echo "Adding dir..."
 
-mkdir "$TARGET (win32)/dir"
-cp -R ../Documentation ../Screenshot ../Source ../README.md "$TARGET (win32)/dir"
-
-mkdir "$TARGET (win64)/dir"
-cp -R ../Documentation ../Screenshot ../Source ../README.md "$TARGET (win64)/dir"
+cp dir.love "$TARGET_WIN32/dir"
+cp dir.love "$TARGET_WIN64/dir"
 
 
-echo "Adding stub launcher..."
+echo "Adding Love2D..."
+
+wget --no-check-certificate "$LOVE_WIN32_URL" -O "love-win32.zip"
+wget --no-check-certificate "$LOVE_WIN64_URL" -O "love-win64.zip"
+
+7z e love-win32.zip -o"$TARGET_WIN32/love2d"
+7z e love-win64.zip -o"$TARGET_WIN64/love2d"
+
+
+echo "Adding launcher..."
 
 /c/MinGW32/bin/windres dir.rc -O coff -o dir_win32.res
-/c/MinGW32/bin/gcc -I /c/MinGW32/include dir.c dir_win32.res -O2 -mwindows -o "$TARGET (win32)/dir.exe"
+/c/MinGW32/bin/gcc -Wall dir.c dir_win32.res -O2 -mwindows -o "$TARGET_WIN32/dir.exe"
 
 /c/MinGW64/bin/windres dir.rc -O coff -o dir_win64.res
-/c/MinGW64/bin/gcc -I /c/MinGW64/include dir.c dir_win64.res -O2 -mwindows -o "$TARGET (win64)/dir.exe"
+/c/MinGW64/bin/gcc -Wall dir.c dir_win64.res -O2 -mwindows -o "$TARGET_WIN64/dir.exe"
 
 
 echo "Compressing..."
 
-7z a "$TARGET (win32).7z" "$TARGET (win32)"
-7z a "$TARGET (win64).7z" "$TARGET (win64)"
+7z a "$TARGET (win32).7z" "$TARGET_WIN32"
+7z a "$TARGET (win64).7z" "$TARGET_WIN64"
 
 
 echo "Cleaning..."
 
+rm dir.love
 rm -rf "$TARGET (win32)" "$TARGET (win64)"
 rm love-win32.zip love-win64.zip
 rm dir_win32.res dir_win64.res
--- Windows/dir.c
+++ Windows/dir.c
@@ -2,14 +2,21 @@
 // dir.
 // A simple, minimalistic puzzle game.
 
+
 // This is a small stub exe to run dir on Windows.
+
+// I prefer to distribute the vanilla Love2D without changes.
+// This launcher changes the icon at runtime and keeps
+// Love2D binaries and dir in separate directories (easier to manage).
 
 
 #include <windows.h>
 
 
-int WINAPI
-WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+// Run a process with a given command-line and return the pid:
+
+DWORD
+RunProcess (char *cmd)
 {
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
@@ -17,8 +24,6 @@ WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCm
     ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
     ZeroMemory(&pi, sizeof(pi));
-
-    char *cmd = "love2d/love.exe dir/Source";
 
     CreateProcess(NULL,  // No module name (use command line).
          cmd,            // Command line.
@@ -30,6 +35,57 @@ WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCm
          NULL,           // Use parent's starting directory.
          &si,            // Pointer to STARTUPINFO structure.
          &pi);           // Pointer to PROCESS_INFORMATION structure.
+
+    return pi.dwProcessId;
+}
+
+
+// Find the handle of a target window by its process id:
+
+DWORD gTargetProcessId = 0;
+HWND gTargetHwnd = NULL;
+
+BOOL CALLBACK myWNDENUMPROC (HWND hwnd, LPARAM lp)
+{
+    if (hwnd == NULL)
+        return TRUE;
+
+    if (!IsWindowVisible(hwnd))
+        return TRUE;
+
+    DWORD dwProcessId;
+    GetWindowThreadProcessId(hwnd, &dwProcessId);
+
+    if (dwProcessId == gTargetProcessId)
+    {
+        gTargetHwnd = hwnd;
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+
+// Entry-point:
+
+int WINAPI
+WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+    // get the icon for this exe (see also dir.rc):
+    HICON icon = (HICON) LoadImage(hInstance, "ID_ICON", IMAGE_ICON, 128, 128, LR_DEFAULTCOLOR | LR_SHARED);
+
+    // launch dir using Love2D:
+    gTargetProcessId = RunProcess("love2d/love.exe dir/dir.love");
+
+    // it can take a while to launch the process, so wait in a loop
+    // until we can get the hwnd:
+    while (gTargetHwnd == NULL) {
+        EnumWindows(myWNDENUMPROC, (LPARAM) NULL);
+        Sleep(0.1);
+    }
+
+    // change the hwnd icon:
+    SendMessage(gTargetHwnd, WM_SETICON, ICON_BIG, (LPARAM) icon);
 
     return 0;
 }
